6468aa828d8c1e521797cd7108c3881cb4e1df8f
Hromt4ik <97237642+Hromt4ik@users.noreply.github.com>
2023-09-29 02:38:53 +0500

    Add files via upload

--- add.php
+++ add.php
@@ -3,30 +3,53 @@ require_once('functions.php');
 require_once('helpers.php');
 require_once('init.php');
 
-
-$errors = [];
-
-    if (isset($_POST["lot-name"]) && empty($_POST['lot-name']))
-    {
-    }
-
-
-
-
-
-function validateFilled($name) {
-    if (empty($_POST[$name])) {
-    return "Поле" . $name . "быть заполнено"; }
-}
-
-function check_correct_date($date): bool{
-
-    return is_date_valid($_POST[$date]);
-}
-
 function getPostVal($name) {
     return $_POST[$name] ?? "";
 }
+
+$errors = [];
+$required_fields =['lot-name','category','message','lot-rate','lot-step','lot-date'];
+
+foreach ($required_fields as $field) {
+    if (!isset($_POST[$field]) || empty($_POST[$field])) {
+        $errors[$field] = 'Поле не заполнено';
+    }
+}
+
+if(!$errors['lot-date']) {
+    if(!is_date_valid($_POST['lot-date']) || (time() >= strtotime($_POST['lot-date']))){
+        $errors['lot-date'] = 'Дата не должна быть пустой или меньше текущей';
+    }
+}
+
+if(!$errors['lot-rate']){
+    if($_POST['lot-rate'] < 0){
+        $errors['lot-rate'] = 'Цена должна быть больше 0';
+    }
+}
+
+if(!$errors['lot-step']){
+    if($_POST['lot-step'] < 0){
+        $errors['lot-step'] = 'Стаквка должна быть больше 0';
+    }
+}
+
+
+if(!$errors['lot-name']){
+    $len = strlen($_POST['lot-name']);
+    if ($len < 3 or $len > 75) {
+        $errors['lot-name'] = "Значение должно быть от 3 до 75 символов";
+    }
+}
+
+if(!$errors['message']){
+    $len = strlen($_POST['message']);
+    if ($len < 3 or $len > 500) {
+        $errors['massage'] = "Значение должно быть от 3 до 500 символов";
+    }
+}
+
+
 
 $page_content = include_template('add-lot.php', [ 'categories' => get_categories($con), 'errors' => $errors]);
 $layout = include_template('layout.php',['title' => 'Главная','is_auth' => $is_auth,
